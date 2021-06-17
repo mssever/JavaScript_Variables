ab5551d6518d9ed1606705706602b55748b6ae37
Scott Severance <scott@scottseverance.us>
2021-06-17 15:21:39 -0500

    operators exercise

---
+++ operators.js
@@ -0,0 +1,53 @@
+// exercise 1
+
+var a = 20, b = 4;
+var add = a + b;
+var minus = a - b;
+var multiply = a * b;
+var dividing = a / b;
+
+//exercise 2
+
+a = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Jackie";
+let lastName = "Chan";
+
+// questions
+
+console.log(a + str); // 22
+console.log(a + str2); // '11eleven'
+console.log(a + isPresent); // 12
+console.log(a + firstName); //'11Jackie'
+console.log(a + lastName); // '11Chan'
+
+
+// exercise 3
+
+let d = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+let firstName2 = "Robin";
+let lastName2 = "Williams";
+
+// What is the value of: d == str3?
+// true
+// What is the value of: d === str3?
+// false
+// What is the value of: !isPresent2?
+// true
+// What is the value of: (“eleven” == str4 && d >= str3)?
+// false
+// What is the value of: (!isPresent2 || isPresent2)?
+// true
+// What is the value of: 0 == false?
+// true
+// What is the value of: 0 === false?
+// false
+// What is the value of: 0 != false?
+// false
+// What is the value of 0 !== false?
+// true
